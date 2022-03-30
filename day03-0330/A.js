f2d2bee29d518d6092607f6d791880a9587382ef
srylove <1535224318@qq.com>
2022-03-30 22:03:39 +0800

    day03

---
+++ day03-0330/A.js
@@ -0,0 +1,78 @@
+/**
+ * @param {number} maxSize
+ */
+ var CustomStack = function (maxSize) {
+  this.MAX = maxSize
+  this.container = []
+};
+
+/** 
+* @param {number} x
+* @return {void}
+*/
+CustomStack.prototype.push = function (x) {
+  if (this.container.length < this.MAX) {
+      this.container.push(x)
+  }
+};
+
+/**
+* @return {number}
+*/
+CustomStack.prototype.pop = function () {
+  if (this.container.length) {
+      return this.container.pop()
+  }
+  return -1
+
+};
+
+/** 
+* @param {number} k 
+* @param {number} val
+* @return {void}
+*/
+CustomStack.prototype.increment = function (k, val) {
+  let len = k > this.container.length ? this.container.length : k
+  for(let i = 0; i<len;i++){
+      this.container[i]+=val
+  }
+};
+
+/**
+* Your CustomStack object will be instantiated and called as such:
+* var obj = new CustomStack(maxSize)
+* obj.push(x)
+* var param_2 = obj.pop()
+* obj.increment(k,val)
+*/
+
+
+
+
+
+/*
+* @param {Array<any>} promises - notice that input might contains non-promises
+* @return {Promise<Array<{status: 'fulfilled', value: any} | {status: 'rejected', reason: any}>>}
+*/
+function allSettled(promises) {
+ // your code here
+ if(promises.length===0) return Promise.resolve([])
+ let res = []
+ return new Promise(resolve => {
+   for(let i = 0;i<promises.length;i++){
+     Promise.resolve(promises[i]).then(r=>{
+       res[i] = {status: 'fulfilled',value: r}
+     }).catch(err=>{
+       res[i] = {status: 'rejected', reason: err}
+     }).finally(()=>{
+       console.log(res.length , promises.length)
+       if(res.length === promises.length){
+         resolve(res)
+       }
+     })
+   }
+   
+ })
+
+}
---
+++ day03-0330/Q.md
@@ -0,0 +1,16 @@
+
+### 1.手写算法
+
+https://leetcode-cn.com/problems/design-a-stack-with-increment-operation/
+
+
+### 2.编程题
+
+https://bigfrontend.dev/zh/problem/implement-Promise-allSettled
+
+Promise.allSettled()方法返回一个在所有给定的promise都已经fulfilled或rejected后的promise，并带有一个对象数组，每个对象表示对应的promise结果。
+
+from MDN
+和Promise.all()不同，Promise.allSettled() 会等待所有的promise直到fulfill或者reject。
+
+你能实现自己的Promise.allSettled() 吗?
