e67bab720e603bf5d2480dfc22a38586029ccd72
srylove <1535224318@qq.com>
2022-03-31 22:34:10 +0800

    day04

---
+++ day04-0331/A.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var decodeString = function (s) {
+  if (s === '') return ''
+  let stack = ['']
+  let str = ''
+  for (let i = 0; i < s.length; i++) {
+      console.log(s[i])
+      if (/[0-9]/.test(s[i])) {
+          if (typeof stack[stack.length - 1] === 'number') {
+              stack.push(stack.pop() * 10 + Number(s[i]))
+          } else {
+              stack.push(Number(s[i]))
+          }
+      }
+      else if (s[i] === '[') {
+          stack.push('')
+      } else if (s[i] === ']') {
+          str = stack.pop().repeat(stack.pop())
+          stack.push(stack.pop()+str)
+      } else {
+          stack.push(stack.pop() + s[i])
+      }
+  }
+  return stack.join('')
+};
+
+
+
+/**
+ * @param {any} a
+ * @param {any} b
+ * @return {boolean}
+ */
+ function is(a, b) {
+  // your code here
+  if(typeof a !== typeof b){
+    return false
+  }
+  if( a.toString() === 'NaN' && b.toString() === 'NaN'){
+    return true
+  }
+  if(typeof a === 'number'){
+    return 1/a === 1/b
+  }
+  return a===b
+}
---
+++ day04-0331/Q.md
@@ -0,0 +1,9 @@
+
+### 1.手写算法
+
+https://leetcode-cn.com/problems/decode-string/
+
+
+### 2.编程题
+
+https://bigfrontend.dev/zh/problem/implement-Object.is
